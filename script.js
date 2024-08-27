403162545c085e8f076d3f494e81edd761f2cf30
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2024-08-26 21:38:08 -0300

    terminei a calculadora

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function insert(num) {
-    document.getElementById("resultado").innerHTML = num
+    let numero = document.getElementById("resultado").innerHTML
+    document.getElementById("resultado").innerHTML = numero + num
+}
+
+function clean() {
+    document.getElementById("resultado").innerHTML = ""
+}
+
+function back() {
+    let resultado = document.getElementById("resultado").innerHTML
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() {
+    let resultado = document.getElementById("resultado").innerHTML
+    if (resultado) {
+        document.getElementById("resultado").innerHTML = eval(resultado) // Eval() serve para resolver calculos matemáticos.
+    }
 }
